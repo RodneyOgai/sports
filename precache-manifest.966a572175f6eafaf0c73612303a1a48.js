33624d2b4bcd8fc771ac9dc4ebd6d2f0cbbb0907
ogairodion1 <JanuaryRivers@yandex.ru>
2019-04-02 16:23:26 +0300

    Updates

---
+++ precache-manifest.966a572175f6eafaf0c73612303a1a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a68ac3ab99b735dd4fc2",
+    "url": "/sports/static/css/main.9a7057dd.chunk.css"
+  },
+  {
+    "revision": "a68ac3ab99b735dd4fc2",
+    "url": "/sports/static/js/main.35f628ee.chunk.js"
+  },
+  {
+    "revision": "318f6e507deae85fccaa",
+    "url": "/sports/static/js/runtime~main.f6ea7a5c.js"
+  },
+  {
+    "revision": "8ff175e8f9d37faea822",
+    "url": "/sports/static/css/2.05f411aa.chunk.css"
+  },
+  {
+    "revision": "8ff175e8f9d37faea822",
+    "url": "/sports/static/js/2.159ce43e.chunk.js"
+  },
+  {
+    "revision": "c3d7d4d896dc14a4db2596457ec89576",
+    "url": "/sports/index.html"
+  }
+];
