429de3257580b263e79b4ee3df89a1fb03ca9516
RodneyOgai <49195404+RodneyOgai@users.noreply.github.com>
2019-04-02 16:02:24 +0300

    Add files via upload

---
+++ precache-manifest.cae43429568cb62c981e31e0b286f081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a68ac3ab99b735dd4fc2",
+    "url": "/static/css/main.9a7057dd.chunk.css"
+  },
+  {
+    "revision": "a68ac3ab99b735dd4fc2",
+    "url": "/static/js/main.35f628ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ff175e8f9d37faea822",
+    "url": "/static/css/2.05f411aa.chunk.css"
+  },
+  {
+    "revision": "8ff175e8f9d37faea822",
+    "url": "/static/js/2.159ce43e.chunk.js"
+  },
+  {
+    "revision": "250b8c7dfec7846fc08f4f4d2467592d",
+    "url": "/index.html"
+  }
+];
